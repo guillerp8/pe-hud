ace311924bae4b01d7eb8607949ea055517a280c
MSRP <OMGmsebastian@gmail.com>
2021-04-28 14:39:03 -0400

    Test

--- server/version_sv.lua
+++ server/version_sv.lua
@@ -3,7 +3,7 @@ local name = "[^4pe-hud^7]"
 AddEventHandler('onResourceStart', function(resourceName)
 	if GetCurrentResourceName() == resourceName then
 		function checkVersion(error, latestVersion, headers)
-			local currentVersion = LoadResourceFile(resourceName, "./server/version")
+			local currentVersion = LoadResourceFile(resourceName, "version")
 			if currentVersion ~= latestVersion and tonumber(currentVersion) < tonumber(latestVersion) then
 				print(name .. " ^1is outdated.\nCurrent version: ^8" .. currentVersion .. "\nNewest version: ^2" .. latestVersion .. "\n^3Update^7: https://github.com/Project-Entity/pe-hud/tree/dev")
 			elseif tonumber(currentVersion) > tonumber(latestVersion) then
