98c505cfe8aed8308f40fdcb88ee4b3d0216a492
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-05-03 14:12:16 -0400

    Update version_sv.lua

--- server/version_sv.lua
+++ server/version_sv.lua
@@ -5,7 +5,7 @@ AddEventHandler('onResourceStart', function(resourceName)
 		function checkVersion(error, latestVersion, headers)
 			local currentVersion = LoadResourceFile(resourceName, "version")
 			if currentVersion ~= latestVersion and tonumber(currentVersion) < tonumber(latestVersion) then
-				print(name .. " ^1is outdated.\nCurrent version: ^8" .. currentVersion .. "\nNewest version: ^2" .. latestVersion .. "\n^3Update^7: https://github.com/Project-Entity/pe-hud/tree/dev")
+				print(name .. " ^1is outdated.\nCurrent version: ^8" .. currentVersion .. "\nNewest version: ^2" .. latestVersion .. "\n^3Update^7: https://github.com/Project-Entity/pe-hud")
 			elseif tonumber(currentVersion) > tonumber(latestVersion) then
 				print(name .. " has skipped the latest version ^2" .. latestVersion .. ". Either Github is offline or the version file has been changed")
 			else
